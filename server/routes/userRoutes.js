2f099a813932f07528efdd79c4004b4e445df2ed
Abhinav Nagar <abhinavnagar26966@gmail.com>
2024-07-10 11:40:55 +0530

    Initial commit

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
 
     try {
+        const userExist = await User.findOne({ email: req.body.email });
+        if (userExist) 
+            return res.status(400).json("User already exists");
 
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+        req.body.password = hashedPassword
         const newUser = new User(req.body)
         await newUser.save()
 
@@ -19,7 +27,35 @@ router.post("/register", async (req, res) => {
 });
 
 router.post("/login", async (req, res) => {
-  
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+        return res.status(400).json("Wrong credentials!");
+    }
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) {
+        return res.status(400).json("Wrong credentials!");
+    }
+    res.status(200).json("Logged in!");
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+router.get("/get-all-users", async (req, res) => {
+  try {
+    const allUsers = await User.find();
+    res.send({
+      success: true,
+      message: "All users have been fetched!",
+      data: allUsers
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: error.message
+    });
+  }
 });
 
 
